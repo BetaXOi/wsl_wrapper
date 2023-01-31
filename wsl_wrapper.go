e34b782df390468f40928afa54173be37c6e4861
Ning Bo <ningb1@chinatelecom.cn>
2023-01-31 09:48:42 +0800

    wrapper `SSH Secure Shell Client` and open WSL terminal tabs in one window

--- wsl_wrapper.go
+++ wsl_wrapper.go
@@ -6,26 +6,37 @@ import (
 	"log"
 	"net"
 	"os/exec"
-	"strings"
+	//"strings"
 	"sync"
 
 	"github.com/hpifu/go-kit/hflag"
 )
 
 // "C:\Program Files\PuTTY\kitty.exe" -t -ssh -load bastion_2018 -pw ****** ningbo@127.0.0.1 -P 53377
+// "C:\Program Files (x86)\SSH Communications Security\SSH Secure Shell\SshClient.exe" -h localhost -p 39464 -u ningbo ningbo@10．246．157．248(SSH).ssh2
 
 func main() {
-	hflag.AddFlag("tty", "request tty", hflag.Shorthand("t"), hflag.Type("bool"))
-	hflag.AddFlag("ssh", "protocol", hflag.Type("bool"))
-	hflag.AddFlag("load", "load session")
-	hflag.AddFlag("pw", "password")
-	hflag.AddFlag("port", "ssh port", hflag.Shorthand("P"))
+	// putty
+	//hflag.AddFlag("tty", "request tty", hflag.Shorthand("t"), hflag.Type("bool"))
+	//hflag.AddFlag("ssh", "protocol", hflag.Type("bool"))
+	//hflag.AddFlag("load", "load session")
+	//hflag.AddFlag("pw", "password")
+	//hflag.AddFlag("port", "ssh port", hflag.Shorthand("P"))
+	//hflag.AddPosFlag("pos", "site")
+
+	// ssh secure shell
+	hflag.AddFlag("host", "ssh remote host", hflag.Shorthand("h"))
+	hflag.AddFlag("port", "ssh port", hflag.Shorthand("p"))
+	hflag.AddFlag("user", "username", hflag.Shorthand("u"))
 	hflag.AddPosFlag("pos", "site")
+
 
 	hflag.Parse()
 
-	site := strings.Split(hflag.GetString("pos"), "@")
-	user := site[0]
+	//site := strings.Split(hflag.GetString("pos"), "@")
+	//user := site[0]
+	user := hflag.GetString("user")
+	remotePort := hflag.GetString("port")
 
 	incoming, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -46,7 +57,7 @@ func main() {
 				log.Fatal("could not accept client connection", err)
 			}
 
-			target, err := net.Dial("tcp", "127.0.0.1:"+hflag.GetString("port"))
+			target, err := net.Dial("tcp", "127.0.0.1:"+remotePort)
 			if err != nil {
 				log.Fatal("could not connect to target", err)
 			}
@@ -69,8 +80,11 @@ func main() {
 	bin := "cmd.exe"
 	cmd := exec.Command(bin)
 	script := "/home/nb/wsl_wrapper.sh " + port + " " + user
-	cmd.Args = append(cmd.Args, "/C", "start", "wt", "bash", "-c", script)
-	//cmd.Args = append(cmd.Args, hflag.Args()...)
+	cmd.Args = append(cmd.Args, "/C", "start", "wt", "-w", "0", "nt", "bash", "-c", script)
+	//bin := `C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.15.3466.0_x64__8wekyb3d8bbwe\wt.exe`
+	//cmd := exec.Command(bin)
+	//script := "/home/nb/wsl_wrapper.sh " + port + " " + user
+	//cmd.Args = append(cmd.Args, "-w", "0", "nt", "bash", "-c", script)
 	fmt.Println(cmd.Args)
 	cmd.Start()
 
